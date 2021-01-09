4f34f379d19843f6dc4661fd912516a49134ba4e
b3z <mayer@zeppel.eu>
2021-01-09 21:52:27 +0100

    Update reventlou from 1.0.0 to 1.1.0

--- Casks/reventlou.rb
+++ Casks/reventlou.rb
@@ -1,12 +1,12 @@
 # typed: false
 cask "reventlou" do
-  version "1.0.0"
-  sha256 "1b20138236e51c3926b2bd92996f23ae0635353f41768337439f4185f089ce6d"
+  version "1.1.0"
+  sha256 "611d46b4d2584d4b6285463e3339a044a89303f22e05d0ec25cb029e7e67f277"
 
   url "https://github.com/b3z/reventlou/releases/download/#{version}/reventlou-#{version}-osx.dmg"
   appcast "https://github.com/b3z/reventlou/releases.atom"
   name "reventlou"
-  desc "Personal database to store all type of things and simply find them again"
+  desc "Personal database to store all type of things and simply find them again."
   homepage "https://github.com/b3z/reventlou"
 
   app "reventlou.app"
