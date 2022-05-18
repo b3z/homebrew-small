1ac72cd6c913a43d277499d4f69a3ecba36b6991
b3z <b3z@zeppel.eu>
2022-05-18 13:07:43 +0200

    add 1.0.1

---
+++ Casks/activate.rb
@@ -0,0 +1,11 @@
+cask "activate" do
+  version "1.0.1"
+  sha256 "e0ac2729acc229dfa34ae875cd1b081f14fdcb52db02420090e196459807be15"
+
+  url "https://github.com/Lakr233/ActivateMac/releases/download/#{version}/Activate.NoDockIcon.zip"
+  name "Activate"
+  desc "Show MS Windows style activation dialog on my screen"
+  homepage "https://github.com/Lakr233/ActivateMac"
+
+  app "Activate.app"
+end
