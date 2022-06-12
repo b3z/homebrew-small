181607677a572c42bd22799c86e7a53d157fab70
b3z <b3z@zeppel.eu>
2022-06-12 07:51:32 +0200

    Update activate.rb

--- Casks/activate.rb
+++ Casks/activate.rb
@@ -1,5 +1,5 @@
 cask "activate" do
-  version "1.0.1"
+  version "1.1.2"
   sha256 "e0ac2729acc229dfa34ae875cd1b081f14fdcb52db02420090e196459807be15"
 
   url "https://github.com/Lakr233/ActivateMac/releases/download/#{version}/Activate.NoDockIcon.zip"
