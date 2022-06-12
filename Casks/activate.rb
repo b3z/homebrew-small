fbb010684e4916c81ad944f53ed1e96c899704a8
b3z <b3z@zeppel.eu>
2022-06-12 08:06:11 +0200

    Update activate.rb

--- Casks/activate.rb
+++ Casks/activate.rb
@@ -1,8 +1,8 @@
 cask "activate" do
   version "1.1.2"
-  sha256 :no_check
+  sha256 
 
-  url "https://github.com/Lakr233/ActivateMac/releases/download/1.1.2/Activate.zip"
+  url "https://github.com/Lakr233/ActivateMac/releases/download/{#version}/Activate.zip"
   name "Activate"
   desc "Show MS Windows style activation dialog on my screen"
   homepage "https://github.com/Lakr233/ActivateMac"
