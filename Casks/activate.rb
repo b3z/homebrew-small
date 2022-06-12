e6196ed078b6b0d569922800edcb15eceb8775f1
b3z <b3z@zeppel.eu>
2022-06-12 08:25:34 +0200

    Update activate.rb

--- Casks/activate.rb
+++ Casks/activate.rb
@@ -3,8 +3,8 @@ cask "activate" do
   sha256 "5433c963e74820ccaf60a3e537c64578ca5cbfd21e93c2f7371a9b79dfced80b"
 
   url "https://github.com/Lakr233/ActivateMac/releases/download/#{version}/Activate.zip"
-  name "Activate"
-  desc "Show MS Windows style activation dialog on my screen"
+  name "activate"
+  desc "Show MS Windows style activation dialog on my screen."
   homepage "https://github.com/Lakr233/ActivateMac"
 
   app "Activate.app"
