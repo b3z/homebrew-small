5f1174d8f5759cba6495a32bfd63d0b5679b89d4
b3z <b3z@zeppel.eu>
2022-06-12 08:08:33 +0200

    Update activate.rb

--- Casks/activate.rb
+++ Casks/activate.rb
@@ -1,6 +1,6 @@
 cask "activate" do
   version "1.1.2"
-  sha256 
+  sha256 "5433c963e74820ccaf60a3e537c64578ca5cbfd21e93c2f7371a9b79dfced80b"
 
   url "https://github.com/Lakr233/ActivateMac/releases/download/#{version}/Activate.zip"
   name "Activate"
